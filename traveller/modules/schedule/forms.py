356e0f884911bedc4a2db565a8376da8b029bf63
Duodu Randy <duodurandy19@gmail.com>
2021-09-07 03:12:08 +0000

    BugFix: Replace Underscore By Space

--- traveller/modules/schedule/forms.py
+++ traveller/modules/schedule/forms.py
@@ -5,6 +5,7 @@ from modules.conf.models import Talk
 from init import ModelForm
 
 import wtforms_alchemy
+from wtforms_components.fields import TimeField
 
 
 class DayForm(ModelForm):
@@ -17,12 +18,16 @@ class NormalActivityForm(ModelForm):
         model = Activity
         exclude = ['talk_id', 'type']
 
+    start_time = TimeField(label="start time")
+    end_time = TimeField(label="end time")
 
 class TalkActivityForm(ModelForm):
     class Meta:
         model = Activity
         exclude = ['talk_id', 'type', 'text']
 
+    start_time = TimeField(label="start time")
+    end_time = TimeField(label="end time")
 
     talks = wtforms_alchemy.fields.QuerySelectMultipleField('Talk', 
         query_factory=lambda: Talk.query.order_by(Talk.title).all()
--- traveller/modules/schedule/models.py
+++ traveller/modules/schedule/models.py
@@ -28,6 +28,7 @@ class Day(PkModel):
 class Activity(PkModel):
     __tablename__ = 'activities'
     
+
     type = db.Column(db.String(300))
     text = db.Column(db.String(300))
     start_time = db.Column(db.Time)
