aa0f10371e8feb795508964f7fa6d47c12721cb7
Isfaaq <isfaaq@bazaks.com>
2021-10-02 23:10:40 +0400

    feat: flask-reuploaded config

--- traveller/init.py
+++ traveller/init.py
@@ -22,6 +22,7 @@ ma = Marshmallow()
 login_manager = LoginManager()
 migrate = Migrate()
 mail = Mail()
+images = UploadSet("images", IMAGES, default_dest=lambda x: "static/images")
 
 from flask_wtf import FlaskForm
 from wtforms_alchemy import model_form_factory
