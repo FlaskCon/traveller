f65ea73d4c4f697a8cc02ba5c8dc8c21a2659d08
Michael Bukachi <michaelbukachi@gmail.com>
2021-10-04 01:08:56 +0300

    fix: Fix broken tests

--- traveller/modules/y/tests/test_y_functional.py
+++ traveller/modules/y/tests/test_y_functional.py
@@ -1,31 +1,20 @@
-import pytest
+import datetime
 
-from modules.conf.models import AuthorList
+from modules.conf.models import Conf, ReviewerList, AuthorList
+from modules.conf.models import Talk
+from modules.schedule.models import Schedule, Day, Activity
 
 
-@pytest.fixture(scope="module", autouse=True)
-def provide_conf_data(db, non_admin_user, conf, last_conf, talk, last_talk, schedule):
-    print(non_admin_user)
-    conf.reviewer_list.reviewers.append(non_admin_user)
-    db.session.add(conf)
-    db.session.add(last_conf)
 
-    talk.create_slug()
-    talk.author_list = AuthorList()
-    talk.author_list.authors.append(non_admin_user)
-    talk.submitter_id = non_admin_user.id
-    talk.talk_conference = conf
-    db.session.add(talk)
-
-    last_talk.create_slug()
-    last_talk.author_list = AuthorList()
-    last_talk.author_list.authors.append(non_admin_user)
-    last_talk.submitter_id = non_admin_user.id
-    last_talk.talk_conference = last_conf
-    db.session.add(last_talk)
-
-    db.session.add(schedule)
-    db.session.commit()
+def create_conf(now, current_year):
+    _30_days_ago = now - datetime.timedelta(days=30)
+    _30_days_from_now = now + datetime.timedelta(days=30)
+    return Conf.create(
+        year=current_year,
+        cfp_start=_30_days_ago.date(),
+        cfp_end=_30_days_from_now.date(),
+        reviewer_list=ReviewerList()
+    )
 
 
 def test_view_landing_page__invalid_year(test_client, current_year):
@@ -34,6 +23,8 @@ def test_view_landing_page__invalid_year(test_client, current_year):
 
 
 def test_view_landing_page(test_client, current_year):
+    now = datetime.datetime.now()
+    conf = create_conf(now, current_year)
     res = test_client.get(f"/y/{current_year}", follow_redirects=True)
     assert res.status_code == 200
     assert f"FLASKCON {current_year}".encode("ascii") in res.data
@@ -53,12 +44,48 @@ def test_view_contact_page(test_client, current_year):
 
 
 def test_view_cfp_page(test_client, login_non_admin_user, current_year):
+    now = datetime.datetime.now()
+    conf = create_conf(now, current_year)
     res = test_client.get(f"/y/{current_year}/cfp", follow_redirects=True)
     assert res.status_code == 200
     assert b"Submit a new talk" in res.data
 
 
-def test_view_profile_page(test_client, login_non_admin_user, current_year, non_admin_user):
+def test_view_profile_page(test_client, login_non_admin_user, current_year, non_admin_user, db_session):
+    non_admin_user = db_session.merge(non_admin_user)
+    now = datetime.datetime.now()
+    last_year = now - datetime.timedelta(days=365)
+    conf = create_conf(now, current_year)
+    last_conf = create_conf(last_year, current_year - 1)
+    talk = Talk.create(
+        id=1,
+        title="Test Talk",
+        summary="Some summary",
+        description="Some Description",
+        level="beginner",
+        year=conf.year,
+        conf_id=conf.id,
+        submitter_id=non_admin_user.id,
+    )
+    talk.create_slug()
+    talk.author_list = AuthorList()
+    talk.author_list.authors.append(non_admin_user)
+    talk.update()
+    old_talk = Talk.create(
+        id=2,
+        title="Test Talk2",
+        summary="Some summary",
+        description="Some Description",
+        level="beginner",
+        year=last_year.year,
+        conf_id=last_conf.id,
+        submitter_id=non_admin_user.id,
+    )
+    old_talk.create_slug()
+    old_talk.author_list = AuthorList()
+    old_talk.author_list.authors.append(non_admin_user)
+    old_talk.update()
+
     res = test_client.get(f"/y/{current_year}/profile", follow_redirects=True)
     assert res.status_code == 200
     assert f"{non_admin_user.email}".encode("ascii") in res.data
@@ -68,7 +95,24 @@ def test_view_profile_page(test_client, login_non_admin_user, current_year, non_
     assert b"Test Talk2" not in res.data
 
 
-def test_view_review_page(test_client, login_non_admin_user, current_year):
+def test_view_review_page(test_client, login_non_admin_user, current_year, non_admin_user, db_session):
+    non_admin_user = db_session.merge(non_admin_user)
+    now = datetime.datetime.now()
+    conf = create_conf(now, current_year)
+    talk = Talk.create(
+        id=1,
+        title="Test Talk",
+        summary="Some summary",
+        description="Some Description",
+        level="beginner",
+        year=conf.year,
+        conf_id=conf.id,
+        submitter_id=non_admin_user.id,
+    )
+    talk.create_slug()
+    talk.author_list = AuthorList()
+    talk.author_list.authors.append(non_admin_user)
+    talk.update()
     res = test_client.get(f"/y/{current_year}/review", follow_redirects=True)
     assert res.status_code == 200
     assert b"Test Talk" in res.data
@@ -77,20 +121,60 @@ def test_view_review_page(test_client, login_non_admin_user, current_year):
     assert b"Test Talk" in res.data
 
 
-def test_view_leaderboard_page(test_client, login_non_admin_user, current_year):
+def test_view_leaderboard_page(test_client, login_non_admin_user, current_year, non_admin_user, db_session):
+    non_admin_user = db_session.merge(non_admin_user)
+    now = datetime.datetime.now()
+    conf = create_conf(now, current_year)
+    talk = Talk.create(
+        id=1,
+        title="Test Talk",
+        summary="Some summary",
+        description="Some Description",
+        level="beginner",
+        year=conf.year,
+        conf_id=conf.id,
+        submitter_id=non_admin_user.id,
+    )
+    talk.create_slug()
+    talk.author_list = AuthorList()
+    talk.author_list.authors.append(non_admin_user)
+    talk.update()
     res = test_client.get(f"/y/{current_year}/leaderboard", follow_redirects=True)
     assert res.status_code == 200
     assert b"Test Talk" in res.data
 
 
 def test_view_schedule_page(test_client, login_non_admin_user, current_year):
+    now = datetime.datetime.now()
+    conf = create_conf(now, current_year)
+    Schedule.create(
+        days=[
+            Day(
+                id=1,
+                date=conf.cfp_end,
+                activities=[
+                    Activity(
+                        type="normal_activity",
+                        text="Fun and Games",
+                        start_time=datetime.time(9, 30),
+                        end_time=datetime.time(10, 0),
+                    )
+                ],
+            ),
+        ],
+        conf_id=conf.id
+    )
     res = test_client.get(f"/y/{current_year}/schedule", follow_redirects=True)
     assert res.status_code == 200
     assert b"Schedule" in res.data
     assert b"Fun and Games" in res.data
 
 
-def test_view_reviewers_page(test_client, login_non_admin_user, current_year):
+def test_view_reviewers_page(test_client, login_non_admin_user, current_year, non_admin_user, db_session):
+    non_admin_user = db_session.merge(non_admin_user)
+    now = datetime.datetime.now()
+    conf = create_conf(now, current_year)
+    conf.reviewer_list.reviewers.append(non_admin_user)
     res = test_client.get(f"/y/{current_year}/reviewers", follow_redirects=True)
     assert res.status_code == 200
     assert b"Reviewers" in res.data
