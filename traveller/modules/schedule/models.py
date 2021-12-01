d8bb08a02833109c397f00928a4c632371fc5078
Randy Duodu <duodurandy19@gmail.com>
2021-12-01 22:30:08 +0000

    Update models.py

--- traveller/modules/schedule/models.py
+++ traveller/modules/schedule/models.py
@@ -45,8 +45,9 @@ class Day(PkModel):
         """
         acts = []
         for act in self.activities:
-            end_datetime = datetime.fromisoformat(f"{self.date} {act.end_time}")
-            start_datetime = datetime.fromisoformat(f"{self.date} {act.start_time}")
+            fmt_str =  r"%Y-%m-%dT%H:%M:%S" # replaces the fromisoformat method, not available in Python 3.6
+            end_datetime = datetime.strptime(f"{self.date}T{act.end_time}", fmt_str)
+            start_datetime = datetime.strptime(f"{self.date}T{act.start_time}", fmt_str)
 
             start_civil_time = datetime(
                 start_datetime.year,
